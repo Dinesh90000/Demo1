a02de9495d5ab760fec4a180695d9f5bac8f589e
Dinesh90000 <dineshsahoo369@gmail.com>
2021-09-10 18:41:00 +0530

    5th commit

---
+++ Demo1/src/Programs/Hello2.java
@@ -0,0 +1,11 @@
+package Programs;
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("comminting 2 nd time");
+
+	}
+
+}
